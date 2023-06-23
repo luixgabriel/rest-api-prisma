6f134969a14265fa1df83003a6e1333ec552d443
luix gabriel <luisgasbriel@gmail.com>
2023-06-23 16:49:12 -0300

    feat: adding cors for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { config } from 'dotenv'
+import cors from 'cors'
 import UserRoutes from '../src/routes/users-routes'
 
 config()
 
 const app = express()
 const port = process.env.PORT
+app.use(cors())
 
 app.use(express.json())
 app.use('/', UserRoutes)
